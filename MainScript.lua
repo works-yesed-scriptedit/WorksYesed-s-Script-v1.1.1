5d26998e011559b8ae02e81f0bff5af1cc7f1298
WorksYesed <joutarou0929@gmail.com>
2025-07-20 21:55:58 +0900

    Update MainScript.lua
    Signed-off-by: WorksYesed <joutarou0929@gmail.com>

--- MainScript.lua
+++ MainScript.lua
@@ -11,7 +11,7 @@ if not success then
 end
 
 -- New draggable Orion Lib script for hub creations!
-local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/refs/heads/main/source')))()
 
 --ウィンドウの作成
 local Window = OrionLib:MakeWindow({
